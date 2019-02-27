ecf5de5f7f0bb6a7f24ee0fe0c9fe606238ace94
Kevin Luna <39965672+KLuna24@users.noreply.github.com>
2019-02-26 22:37:56 -0500

    Update CrackHash.py

--- CrackHash.py
+++ CrackHash.py
@@ -13,25 +13,24 @@ import time
 import hashlib 
 
 def brute_hash():
-
-    # The passwords list in txt file
+    
     with open('password.txt', 'r') as passwords:
         passwords_list = passwords.read().splitlines()
 
-    # keeps track of the number of comparisons
+    
     count = 0
 
-    # store the string form of the salt.
+   
     concat = ""
 
-    # starts the timer.
+   
     start = time.time()
-    #checks to see if theres salt and stores string
+   
     if len(sys.argv) == 3:
         for password in passwords_list:
             if hashlib.sha1(password).hexdigest() == sys.argv[2]:
                 concat = str(password)
-#concate salt after password
+
     for password in passwords_list:
         if hashlib.sha1(password + concat).hexdigest() == sys.argv[1]:
             end = time.time()
@@ -39,7 +38,7 @@ def brute_hash():
             quit()
         else:
             count += 1
-#concate the salt before password and checks to see if passed hash matches
+
     for password in passwords_list:
         if hashlib.sha1(concat + password).hexdigest() == sys.argv[1]:
             end = time.time()
@@ -48,7 +47,7 @@ def brute_hash():
         else:
             count += 1
 
-    # Loop checks for all possible combinations
+   
     for password in passwords_list:
         for secondPassword in passwords_list:
             if hashlib.sha1(password + ' ' + secondPassword).hexdigest() == sys.argv[1]:
@@ -57,7 +56,7 @@ def brute_hash():
                 quit()
             else:
                 count += 1
-#If theres no hash print message
+
 def main():
 
     if len(sys.argv) < 2:
